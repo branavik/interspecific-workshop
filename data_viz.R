df8f9e987f82925c12680ab08e1c951d7e3206fe
nicolarammell <nicolarammell@gmail.com>
2026-04-28 10:22:09 -0400

    started script

---
+++ data_viz.R
@@ -0,0 +1,27 @@
+## Script: data_viz.R
+## Interspecific Workshop
+## Authors: NR & BK (2026)
+## Forest Ecology Research Lab
+## Wilfrid Laurier University
+
+# This script is used to visualize interspecific tree age data from 2024/2025 sites
+# sampled in Yukon Territory. 
+
+# LOAD LIBRARIES ----
+
+# load libraries
+library(here)      # for relative working directory 
+library(tidyverse) # dplyr, forcats, gglot2, lubridate, purrr, readr, stringr, tibble, tidyr
+library(leaflet)   # mapping sites
+library(sf)        # mapping sites
+library(ggridges)  # for making interspecific plot
+library(dplR)      # dendro
+library(treeclim)  # dendro
+library(TRADER)    # dendro
+library(graphics)  # dendro
+library(utils)     # dendro
+
+# set theme
+theme_set(theme_classic())
+
+# READ IN DATA ----
